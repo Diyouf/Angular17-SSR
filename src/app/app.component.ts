1d1531101abc76c9c7e223c9656da7e9c99f2f41
Diyouf <diyoufkv7@gmail.com>
2024-03-13 13:43:31 +0530

    Angular17(SSR):Testing and configration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,59 @@
+
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular17SSR';
+  title !: string
+  isVisible:boolean = false
+  constructor(private router: Router) { }
+
+  data = [
+    {
+      "company": "ABC Inc.",
+      "contact": "John Smith",
+      "country": "United States"
+    },
+    {
+      "company": "XYZ Ltd.",
+      "contact": "Jane Doe",
+      "country": "Canada"
+    },
+    {
+      "company": "123 GmbH",
+      "contact": "Max Mustermann",
+      "country": "Germany"
+    },
+    {
+      "company": "Tech Solutions Co.",
+      "contact": "Emily Johnson",
+      "country": "United Kingdom"
+    },
+    {
+      "company": "Global Innovations",
+      "contact": "Carlos Sanchez",
+      "country": "Spain"
+    },
+    {
+      "company": "Pacific Ventures",
+      "contact": "Li Wei",
+      "country": "China"
+    }
+  ]
+
+  getInputData(event: Event) {
+    this.title = (event.target as HTMLInputElement).value ?? 'hai'
+  }
+
+  onUserNav() {
+    this.isVisible = !this.isVisible
+  }
 }
