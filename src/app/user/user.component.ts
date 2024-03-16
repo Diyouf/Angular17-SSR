b3abe8c4ea3c53e96a98b6e5e91bf99680282090
Diyouf <diyoufkv7@gmail.com>
2024-03-16 21:30:34 +0530

    Angular17(SSR):ClientSideHydration checked

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,14 +1,40 @@
-import { Component, Input } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { ServiceService } from '../service.service';
+import { delay } from 'rxjs';
+import { error } from 'console';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
+  providers: [HttpClientModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
-  @Input() fromParent : any = []
-  @Input() title!:string 
+export class UserComponent implements OnInit {
+  fromParent !: any
+  @Input() title!: string
+
+  constructor(private service: ServiceService) { }
+
+  ngOnInit() {
+    this.getdata()
+  }
+
+
+  getdata() {
+    this.service.getJsonData().subscribe(
+      (res) => {
+        this.fromParent = res;
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+      }
+    );
+  }
+  
+
+
 
 }
